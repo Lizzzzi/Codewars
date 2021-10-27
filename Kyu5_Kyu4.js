6717b4de27b9ba16e151142c550ab35dd090f2a1
Lizzzzi <jelizaveta.stolce@gmail.com>
2021-10-26 20:18:44 -0600

    5 kyu task

--- Kyu5_Kyu4.js
+++ Kyu5_Kyu4.js
@@ -22,3 +22,26 @@ console.log( postfixEvaluator("2 3 +")); //  5
 
 console.log("------------- NEXT KATA -----------" )
 
+// 5 kyu   https://www.codewars.com/kata/561e9c843a2ef5a40c0000a4
+
+function gap(g, m, n) {
+    if (g % 2) return null;
+    let firstNum = 0;
+    first: for (let i = m; i <= n; i++) {
+        if (i % 2) {
+            let sqr = Math.sqrt(i);
+            for (let j = 3; j <= sqr; j += 2 ) {
+                if (i % j === 0) continue first;
+            }
+            if (i - firstNum === g) return [firstNum, i];
+            else firstNum = i;
+        }
+    }
+    return null;
+}
+
+console.log(gap(2,100,110)); // [101, 103]);
+console.log(gap(4,100,110)); // [103, 107]);
+console.log(gap(6,100,110)); // null);
+console.log(gap(8,300,400)); // [359, 367]);
+console.log(gap(10,300,400)); // [337, 347]);
